ff18f9c8e92fc713f4316a45c0d00ebe97094bc3
codedev101 <87523825+codedev101@users.noreply.github.com>
2025-09-20 06:04:21 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,8 @@ st.markdown("[![Source](https://img.shields.io/badge/View-Source-blue.svg)](http
 def get_driver():
     """Initialize Chrome driver with proper options for Streamlit Cloud"""
     options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-web-security")
+
+
     
     try:
         driver = webdriver.Chrome(
